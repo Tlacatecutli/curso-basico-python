7b642e28787325c55caab568f156281b89b6f6a2
Tlacatecutli Navarro <tlacatecutli.navarro@gmail.com>
2022-06-01 12:58:34 -0500

    Initial commit

---
+++ tiposvariables.py
@@ -0,0 +1,11 @@
+#String - cadena de texto
+nombre = "Tlacatecutli"
+
+#Enteros - Numero sin punto decimal
+edad = 20
+
+#Flotante - Numero con punto decimal
+
+pi = 3.1416
+
+edad = float(edad) #Casteo - Transformar un tipo de variable a otro
